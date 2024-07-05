7d4c9d1a88e8854a142f52f5af5c1bf710665648
dongganxixi <ruixing.xixi@gmail.com>
2024-07-05 08:56:48 +0800

    Initial Commit

---
+++ SwiftulCypto/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftulCypto
+//
+//  Created by 胡瑞兴 on 2024/7/5.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftulCypto/SwiftulCyptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftulCyptoApp.swift
+//  SwiftulCypto
+//
+//  Created by 胡瑞兴 on 2024/7/5.
+//
+
+import SwiftUI
+
+@main
+struct SwiftulCyptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
